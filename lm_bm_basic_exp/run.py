7b058cb5126439afc9939e6dbf15bf7c389cf289
Caner <caneris1992@gmail.com>
2020-04-07 10:29:47 +0200

    building a stepfunction

--- lm_bm_basic_exp/run.py
+++ lm_bm_basic_exp/run.py
@@ -3,16 +3,19 @@ from plot_tool import plot_lm
 
 
 
-T = 500
+T = 200
 rd.seed(135)
 m = Model(T=T, alpha_2=0.1, chi_C=0.4)
 
 # initialize employment
 initialize_emp(m.h_arr, m.f_arr, m.F, int(m.Nr), int(m.Nnr))
 
+for t in range(m.T):
+    m.step_function()
+
 set_W_fs(m.f_arr, m.h_arr)
 val = 0
 
-f1, f2 = plot_lm(m, 500, 500)
+f1, f2 = plot_lm(m, m.T, m.T)
 f1.show()
 f2.show()
--- lm_bm_basic_exp/stepfunction_methods.py
+++ lm_bm_basic_exp/stepfunction_methods.py
@@ -44,3 +44,67 @@ def firm_decisions(m):
     update_m(m.f_arr[m.active_fs], m.sigma_chi)  # choose markup
     update_uc_arr(m.f_arr, m.t)
     update_p(m.f_arr[m.active_fs], m.t)
+
+
+def run_labor_market(m):
+
+    ## Labor market matching
+    # get vacancies Fx2 matrix
+    v_mat = np.array([(f.id, f.v_r, f.v_nr) for f in m.f_arr[m.active_fs]])
+    firms_fire_r_workers(v_mat, m.h_arr, m.f_arr, m.t)
+    firms_fire_nr_workers(v_mat, m.h_arr, m.f_arr, m.t)
+    update_N(m.f_arr[m.active_fs])
+
+    # households apply
+    hs_send_nr_apps(m.f_arr, m.h_arr[m.non_routine_arr], m.chi_L, m.H_nr, m.H_r, m.beta)
+    hs_send_r_apps(m.f_arr, m.h_arr[m.routine_arr], m.chi_L, m.H_r, m.beta)
+
+    # firms hire
+    firms_employ_nr_applicants(m.f_arr, m.h_arr, m.lambda_LM, m.min_w, m.t)
+    update_N(m.f_arr)
+    set_W_fs(m.f_arr, m.h_arr)
+
+    firms_employ_r_applicants(m.f_arr, m.h_arr, m.lambda_LM, m.min_w, m.t)
+    update_N(m.f_arr)
+    set_W_fs(m.f_arr, m.h_arr)
+
+    clear_applications(m.f_arr)
+
+
+def run_goods_market(m):
+
+    # firms produce goods
+    firms_produce(m.f_arr, m.mu_r, m.mu_nr, m.sigma)
+
+    # firms sell goods
+    clear_s(m.f_arr)
+    clear_expenditure(m.h_arr)
+    gm_matching(m.f_arr, m.h_arr, m.chi_C, m.tol)
+    update_inv(m.f_arr[m.active_fs])
+
+    # households update mean prices
+    update_h_p(m.h_arr)
+
+
+def firm_profits_and_dividends(m):
+
+    # firms calculate profits
+    update_pi(m.f_arr)
+    update_div_f(m.f_arr[m.active_fs])
+
+    # surviving firms pay dividends
+    distribute_dividends(m.h_arr, m.f_arr)
+
+
+def hh_refin_firms(m):
+
+    # households refinance firms
+    m.active_fs = surviving_firms(m.f_arr)
+    m.default_fs = default_firms(m.f_arr)
+
+    mean_Af = np.mean(np.array([f.A for f in m.f_arr[m.active_fs]]))
+    refin_firms(mean_Af, m.f_arr[m.default_fs], m.f_arr[m.active_fs], m.h_arr, m.n_refinanced,
+                m.tol, m.t)
+
+    m.active_fs = surviving_firms(m.f_arr)
+    m.default_fs = default_firms(m.f_arr)
