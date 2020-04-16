9fbcb0c8d7eee4ff0b8778d8074fa712df4f23e5
Caner <caneris1992@gmail.com>
2020-04-16 15:56:10 +0200

    tried out results

--- lm_bm_basic_exp/run.py
+++ lm_bm_basic_exp/run.py
@@ -4,9 +4,9 @@ from plot_tool import plot_lm
 
 
 T = 500
-rd.seed(12343)
-m = Model(T=T, alpha_2=0.1, chi_L=0.4, chi_C=0.4, lambda_LM=10, sigma_m=0.001, sigma_w= 0.005,
-          nu=0.1, u_r=0.10, beta=1, lambda_exp = 0.5)
+rd.seed(334421457)
+m = Model(T=T, alpha_2=0.1, chi_L=0.4, chi_C=0.4, lambda_LM=3, sigma_m=0.001, sigma_w= 0.005,
+          nu=0.1, u_r=0.10, beta=1, lambda_exp = 0.5, F = 40, H = 250)
 
 m.run()
 
