3df38b37d4f8dcba41056dbddd3770ffa1d51db9
atecan00 <c.ates@unibas.ch>
2020-06-17 16:47:30 +0200

    adjusted update_d_w

--- lm_bm_basic_exp/run.py
+++ lm_bm_basic_exp/run.py
@@ -8,7 +8,7 @@ import time
 
 T = 500
 periods = T
-rd.seed(13521332)
+rd.seed(135232)
 m = Model(T=T, alpha_2=0.5, chi_C=0.1, lambda_LM=1, sigma_m=0.2, sigma_w= 0.05, sigma_delta=0.001,
           nu=0.1, u_r=0.05, beta=1, lambda_exp = 0.5, F = 40, H = 250, N_app=4, sigma=1.5, mu_r=0.33,
           nr_to_r=True, a = 100, shock_t=0, min_realw_t=None, gamma_nr=0.33, W_r=50, minw_init_par = 0.4)
@@ -22,7 +22,7 @@ print(np.mean(m.mean_p_arr[400:]))
 
 fig1, fig2 = plot_lm(m, m.t, m.t, 1)
 fig3 = get_wage_dist_fig(m)
-fig4 = get_aggregate_regs(m, m.t, 100)
+fig4 = get_aggregate_regs(m, m.t, 200)
 
 fig1.show()
 plt.close(fig1)
