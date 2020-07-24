62fb025a286a0bbf168e432c62216034c3fc0dd3
atecan00 <c.ates@unibas.ch>
2020-07-24 15:22:26 +0200

    changed pre-test

--- lm_bm_basic_exp/pre_test.py
+++ lm_bm_basic_exp/pre_test.py
@@ -17,17 +17,20 @@ def run_perms(ID, NC, T):
         N_app = rd.randint(2,11)
         N_good = rd.randint(2,11)
         lambda_LM = rd.randint(2, 11)
-        sigma_w = rd.uniform(0.05, 0.5)
-        min_w_par = rd.uniform(0.1, 0.5)
-        sigma = rd.choice(np.linspace(0.5, 1.5, num=6),1)[0]
+        sigma_w = rd.uniform(0.1, 0.5)
+        sigma_m = rd.uniform(0.1, 0.5)
+        min_w_par = rd.uniform(0.01, 0.6)
 
-        m = Model(T=T, alpha_2=0.25, sigma=sigma, N_app=N_app, N_good=N_good, lambda_LM=lambda_LM, sigma_m=0.1,
-                  sigma_w= sigma_w, nu = 0.1, u_r=0.08, beta=1, lambda_exp = 0.5, F = 80, H = 500, min_w_par=min_w_par)
+        m = Model(T=T, alpha_2=0.25, sigma=1.5, N_app=N_app, N_good=N_good, lambda_LM=lambda_LM, sigma_m=sigma_m,
+                  sigma_w= sigma_w, nu = 0.1, u_r=0.08, beta=1, lambda_exp = 0.5, F = 80, H = 500, min_w_par=min_w_par,
+                  nr_to_r=False, mu_r = 0.4, gamma_nr = 0.4, sigma_delta=0.001, a = 1, f_max=1, W_r=1)
         m.run()
 
         with open('pretest_unemp_ID{}.csv'.format(ID), 'a', newline='') as csvfile:
             filewriter = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
             filewriter.writerow(m.u_r_arr)
+
+        print("sim {} finished")
 
 
 def run_nc_with_mp(args_arr):
